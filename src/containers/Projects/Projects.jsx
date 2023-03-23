b881d89a40a5be51cf43b93c282c0444bd19f28b
semako123 <zosusemenoch@gmail.com>
2023-03-23 21:25:56 +0100

    Add spoonacular to projects

--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -11,9 +11,9 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 const Projects = () => {
-  const width = window.innerWidth;
+	const width = window.innerWidth;
 
-  return (
+	return (
 		<div className="oases__projects section__padding" id="projects">
 			<AnimationOnScroll
 				animateIn="animate__fadeInDown"
@@ -52,7 +52,7 @@ const Projects = () => {
 								title="GPT-3"
 								img={gpt}
 								note="GPT-3 is a website for an OpenAI. It features great layout, responsiveness and gradient design"
-								tech="React,CSS, JavaScript"
+								tech="React, CSS, JavaScript"
 								code_link="https://github.com/semako123/gpt3"
 								live_link="https://oasesgpt3.netlify.app"
 							/>
@@ -72,7 +72,7 @@ const Projects = () => {
 								title="O-Movies"
 								img={omovies}
 								note="O-Movies is a web-application where you can get the detail of any movie, series or anime of your choice, It features great and easy-to-use UI, a search function, different section to explore different categories of movies."
-								tech="React, CSS, JavaScript, REST API, Redux"
+								tech="React, CSS, JavaScript, REST API, Redux, Axios"
 								code_link="https://github.com/semako123/omovies"
 								live_link="https://oasesmovies.netlify.app"
 							/>
@@ -85,6 +85,16 @@ const Projects = () => {
 								tech="React, CSS, JavaScript, React-Routers, Animate.css, Animation on scroll"
 								code_link="https://github.com/semako123/ghit"
 								live_link="https://ghitechnologies.netlify.app"
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<Project
+								title="Spoonacular"
+								img={growfy}
+								note="Spoonacular is a recipe web application where you can explore various recipes to become the chef of your kitchen"
+								tech="React, CSS, JavaScript, React-routers, RESTful API, Axios"
+								code_link="https://github.com/semako123/spoonacular"
+								live_link="https://oasesfood.netlify.app"
 							/>
 						</SwiperSlide>
 					</Swiper>
